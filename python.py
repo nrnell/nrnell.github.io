bc749f0c2d0865737a30937565677135452915ce
Nёll <kazuki_osada@outlook.jp>
2024-09-05 21:19:28 +0900

    Commit

--- python.py
+++ python.py
@@ -1,3 +1,21 @@
-import discord
+from dotenv import load_dotenv
+load_dotenv()
 
-print('Python')
+import discord
+import os
+
+client = discord.Client(intents=discord.Intents.all())
+nomention = discord.AllowedMentions.none()
+discord_avatar = 'https://cdn.discordapp.com/embed/avatars/0.png'
+
+@client.event
+async def on_ready():
+
+    try:
+        print('Success')
+    except:
+        pass
+
+# client.run(os.getenv('TOKEN'))
+
+print(os.getenv('TOKEN'))
