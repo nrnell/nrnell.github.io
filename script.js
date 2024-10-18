3553e16b2248551a9840b1205068239de90bd53d
Nёll <kazuki_osada@outlook.jp>
2024-10-18 20:36:17 +0900

    Commit

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 document.querySelector('footer').style.height = document.querySelector('header').offsetHeight+'px';
 document.querySelector('header').style.height = document.querySelector('footer').offsetHeight+'px';
 document.querySelector('header').style.fontSize = document.querySelector('header').offsetHeight/3+'px';
+document.querySelector('footer').style.gap = document.querySelector('header').offsetHeight/4+'px';
 document.querySelector('main').style.paddingTop = document.querySelector('header').offsetHeight+'px';
 document.querySelector('.slider').style.height = document.querySelector('.slider').offsetWidth/3+'px';
 document.querySelectorAll('footer svg').forEach(function(element){
     element.style.width = document.querySelector('header').offsetHeight/4+'px';
     element.style.height = document.querySelector('header').offsetHeight/4+'px';
-    element.style.padding = document.querySelector('header').offsetHeight/8+'px';
 });
-document.querySelectorAll('.slider .banner').forEach(function(element){
-    element.style.width = document.querySelector('.slider').offsetWidth+'px';
-    element.style.height = element.offsetWidth/3+'px';
-});
-// window.onresize = function(){location.reload()};
 $('.slider').slick({
     autoplay:true,
     autoplaySpeed:3000,
@@ -25,7 +20,7 @@ $('.slider').slick({
     pauseOnFocus:false,
     pauseOnHover:false,
 });
-
+//window.onresize = function(){location.reload()};
 // let local = {};
 // if(!localStorage.hasOwnProperty('local')){localStorage.setItem('local',JSON.stringify(local));};
 // local = JSON.parse(localStorage.getItem('local'));
