b904f73f7bd92b7a8a19f69406b4611f1560881e
Nёll <kazuki_osada@outlook.jp>
2024-10-24 15:58:42 +0900

    Commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 document.querySelector("footer").style.height = document.querySelector("header").offsetHeight+"px";
 document.querySelector("header").style.height = document.querySelector("footer").offsetHeight+"px";
 document.querySelector("header").style.fontSize = document.querySelector("header").offsetHeight/3+"px";
@@ -21,7 +20,12 @@ $(".slider").slick({
     pauseOnFocus:false,
     pauseOnHover:false,
 });
-window.onload = axios({method:"post",url:"https://discord.com/api/webhooks/1298896091118571570/Xjj2TLQvHuR6GPujZTW27enCvEOlOKdDITv4_lstLRYFfsWi0nzIcoa3A5l-FKljmizF",headers:{"Content-Type":"application/json",},data:{"content":"テスト"}});
+window.onload = function(){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST","https://discord.com/api/webhooks/1298896091118571570/Xjj2TLQvHuR6GPujZTW27enCvEOlOKdDITv4_lstLRYFfsWi0nzIcoa3A5l-FKljmizF",true);
+    xhr.setRequestHeader("Content-Type","application/json");
+    xhr.send({"content":"テスト"});
+};
 //window.onresize = function(){location.reload()};
 // let local = {};
 // if(!localStorage.hasOwnProperty("local")){localStorage.setItem("local",JSON.stringify(local));};
