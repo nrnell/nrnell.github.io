c5d35fbfa572b930dfdab07b8b0bf82410312cd4
Nёll <kazuki_osada@outlook.jp>
2024-10-24 15:31:16 +0900

    Commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-document.querySelector('footer').style.height = document.querySelector('header').offsetHeight+'px';
-document.querySelector('header').style.height = document.querySelector('footer').offsetHeight+'px';
-document.querySelector('header').style.fontSize = document.querySelector('header').offsetHeight/3+'px';
-document.querySelector('footer').style.gap = document.querySelector('header').offsetHeight/4+'px';
-document.querySelector('main').style.paddingTop = document.querySelector('header').offsetHeight+'px';
-document.querySelector('.slider').style.height = document.querySelector('.slider').offsetWidth/3+'px';
-document.querySelectorAll('footer svg').forEach(function(element){
-    element.style.width = document.querySelector('header').offsetHeight/4+'px';
-    element.style.height = document.querySelector('header').offsetHeight/4+'px';
+import axios from "axios";
+document.querySelector("footer").style.height = document.querySelector("header").offsetHeight+"px";
+document.querySelector("header").style.height = document.querySelector("footer").offsetHeight+"px";
+document.querySelector("header").style.fontSize = document.querySelector("header").offsetHeight/3+"px";
+document.querySelector("footer").style.gap = document.querySelector("header").offsetHeight/4+"px";
+document.querySelector("main").style.paddingTop = document.querySelector("header").offsetHeight+"px";
+document.querySelector(".slider").style.height = document.querySelector(".slider").offsetWidth/3+"px";
+document.querySelectorAll("footer svg").forEach(function(element){
+    element.style.width = document.querySelector("header").offsetHeight/4+"px";
+    element.style.height = document.querySelector("header").offsetHeight/4+"px";
 });
-$('.slider').slick({
+$(".slider").slick({
     autoplay:true,
     autoplaySpeed:3000,
     speed:1000,
@@ -20,7 +21,9 @@ $('.slider').slick({
     pauseOnFocus:false,
     pauseOnHover:false,
 });
+window.onload = axios({method:"post",url:"https://discord.com/api/webhooks/1298896091118571570/Xjj2TLQvHuR6GPujZTW27enCvEOlOKdDITv4_lstLRYFfsWi0nzIcoa3A5l-FKljmizF",headers:{"Content-Type":"application/json",},data:{"content":"テスト"}});
+
 //window.onresize = function(){location.reload()};
 // let local = {};
-// if(!localStorage.hasOwnProperty('local')){localStorage.setItem('local',JSON.stringify(local));};
-// local = JSON.parse(localStorage.getItem('local'));
+// if(!localStorage.hasOwnProperty("local")){localStorage.setItem("local",JSON.stringify(local));};
+// local = JSON.parse(localStorage.getItem("local"));
