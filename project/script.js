f0bd4e02ae2cdbc1ea33f4ccb34944c381257dad
Nёll <kazuki_osada@outlook.jp>
2024-11-11 19:59:43 +0900

    Commit

--- project/script.js
+++ project/script.js
@@ -1,1 +1,6 @@
-d3.csv("https://docs.google.com/spreadsheets/d/1e3fnVHZJwgZs2d06o05_bzvfef2Z5ALpJnBfvyaqGyA/export?format=csv&range=A3:D",function(error,data){var main="";for(var i=0;i<data.length;i++){console.log(data[i].username);main+=i+1+" 名前: "+data[i].username+" "+data[i].timestamp+"<pre>"+data[i].message+"</pre>";}d3.select("main").html(main);});
+d3.csv("https://docs.google.com/spreadsheets/d/1e3fnVHZJwgZs2d06o05_bzvfef2Z5ALpJnBfvyaqGyA/export?format=csv&range=A3:D",function(error,data){
+    //var main="";
+    //for(var i=0;i<data.length;i++){main+=i+1+" 名前: "+data[i].username+" "+data[i].timestamp+"<pre>"+data[i].message+"</pre>";}
+    //d3.select("main").html(main);
+    console.log(data);
+});
