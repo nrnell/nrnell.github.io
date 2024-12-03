7ab4f6d902d00bec2fefe08364f1393e32da6c14
Nёll <kazuki_osada@outlook.jp>
2024-12-03 21:56:30 +0900

    Commit

--- channel/script.js
+++ channel/script.js
@@ -10,7 +10,7 @@ const loading = () => {
         var comment = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1g7dS6R2nKWgLN_x8V3r9Q9Rfl51SFfkyDCIVuT15i-8/values/Responses?key=AIzaSyDh5OCWGGIs9l7q2L6zszjEklNGOyThr1M");
         comment = await comment.json();
         comment = comment.values;
-        for (let i=1;i<comment.length;i++) {console.log(i);main+=`<b style="color:${comment[i][3]};">${comment[i][2]}</b><pre>${comment[i][4]}</pre>`;};
+        for (let i=1;i<comment.length;i++) {main+=`<b style="color:${comment[i][3]};">${comment[i][2]}</b><pre>${comment[i][4]}</pre>`;};
         document.querySelector("main").innerHTML = main;
         loading()
     },1000)
