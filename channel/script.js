e20eac5234759ca66faba56fa70a8fa354194d71
Nёll <kazuki_osada@outlook.jp>
2024-12-04 01:04:47 +0900

    Commit

--- channel/script.js
+++ channel/script.js
@@ -1,13 +1,14 @@
-let local = {"uuid":crypto.randomUUID(),"name":""};
-//if (!localStorage.hasOwnProperty("local")) {while(!local.name||local.name.length>32){local.name=window.prompt("ユーザー名");};localStorage.setItem("local",JSON.stringify(local));};
-if (!localStorage.hasOwnProperty("local")) {localStorage.setItem("local",JSON.stringify(local));};
-local = JSON.parse(localStorage.getItem("local"));
+const version = "β";
+var latest = {"ver":version,"uuid":crypto.randomUUID()};
+if (!localStorage.hasOwnProperty("local")) {localStorage.setItem("local",JSON.stringify(latest));};
+var local = JSON.parse(localStorage.getItem("local"));
+if (!local.ver||local.ver!=version) {local=Object.assign(latest,local);local.ver=version;};
 while (!local.name||local.name.length>32) {local.name=window.prompt("ユーザー名");};
 localStorage.setItem("local",JSON.stringify(local));
 document.querySelectorAll("#uuid").forEach((element) => element.value = local.uuid);
 document.querySelectorAll("#name").forEach((element) => element.value = local.name);
 function submitted() {document.querySelectorAll("#content").forEach((element) => element.value = "");};
-const loading = () => {
+function loading() {
     setTimeout(async () => {
         var main = "";
         var comment = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1g7dS6R2nKWgLN_x8V3r9Q9Rfl51SFfkyDCIVuT15i-8/values/Responses?key=AIzaSyBRBSvIUhGf7hbYmcEz70aMNJWcxKq0ZZA");
