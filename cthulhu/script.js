fcac749885e61786e20fa0e66b83aadec29e5713
Nёll <kazuki_osada@outlook.jp>
2025-01-29 22:31:06 +0900

    Commit

--- cthulhu/script.js
+++ cthulhu/script.js
@@ -1,5 +1,5 @@
 var pre = document.querySelector("pre");
 pre.innerHTML = pre.innerHTML.replaceAll(/\|([^|《》]+)《([^|《》]+)》/g,`<ruby ruby="$2">$1</ruby>`);
-pre.innerHTML = pre.innerHTML.replaceAll(/#([^#]+)#/g,`<a href="#$1">$1</a>`)
 document.querySelectorAll('h1,h2,h3,span:not([id])').forEach(element=>element.id=element.innerText);
-document.querySelectorAll('[id]').forEach(element=>element.innerHTML=`<a href="#${element.id}">${element.innerText}</a>`)
+document.querySelectorAll('[id]').forEach(element=>element.innerHTML=`<a href="#${element.id}">${element.innerText}</a>`);
+document.querySelectorAll('[id]').forEach(element=>pre.innerHTML=pre.innerHTML.replaceAll(`#${element.id}`,`<a href="#${element.id}">${element.id}</a>`));
